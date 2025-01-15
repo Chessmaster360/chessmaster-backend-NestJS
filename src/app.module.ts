2463bd591608c8120af88955107ef92a17acce15
geoffrey0pv <geoffreypv00@gmail.com>
2025-01-14 23:34:56 -0500

    fix: try to fix mongo connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,9 +8,7 @@ import { BotsModule } from './bots/bots.module';
 @Module({
   imports: [
     ConfigModule.forRoot(), // Carga las variables de entorno
-    MongooseModule.forRoot(process.env.MONGO_URI, {
-      dbName: 'chessmaster', // Nombre de la base de datos
-    }),
+    MongooseModule.forRoot(process.env.MONGO_URI),
     UsersModule,
     ChessModule,
     BotsModule, // Módulo de los servicios de ajedrez
