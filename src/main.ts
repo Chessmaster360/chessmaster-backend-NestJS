012da117ebc77b456c3be61807dbeb87169ceb7d
geoffrey0pv <geoffreypv00@gmail.com>
2025-01-14 19:59:53 -0500

    fix: cors

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
-   // Establece un prefijo global para las rutas
-   app.setGlobalPrefix('api');
+  // Establece un prefijo global para las rutas
+  app.setGlobalPrefix('api');
 
-   // Configuración de CORS
-    app.enableCors({
-      origin: 'http://localhost:5173', // Origen permitido
-      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Métodos HTTP permitidos
-      credentials: true, // Habilitar envío de cookies si es necesario
-    });
+  // Configuración de CORS
+  app.enableCors({
+    origin: [
+      'http://localhost:5173', // Ambiente local
+      'https://chessmaster360.netlify.app', // URL de producción
+    ], // Permitir múltiples orígenes
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Métodos HTTP permitidos
+    credentials: true, // Permitir envío de cookies si es necesario
+  });
 
-
-   // Inicia el servidor
-   await app.listen(process.env.PORT ?? 3000);
+  // Inicia el servidor
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
