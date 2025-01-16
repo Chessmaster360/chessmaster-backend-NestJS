f9308ad26ca3447bea5d21f206e083d8b6120964
geoffrey0pv <geoffreypv00@gmail.com>
2025-01-15 19:16:58 -0500

    fix: config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,33 +1,20 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { ChessModule } from './chess/chess.module';
 import { BotsModule } from './bots/bots.module';
 
 @Module({
   imports: [
-    // ConfigModule cargará la configuración adecuada según NODE_ENV
-    ConfigModule.forRoot({
-      envFilePath: [
-        `.env`, // Archivo genérico
-        `src/config/${process.env.NODE_ENV || 'development'}.env`, // Según el entorno
-      ],
-      isGlobal: true, // Hace accesible la configuración globalmente
-    }),
-    // Configura Mongoose dinámicamente según la configuración
-    MongooseModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => ({
-        uri: configService.get<string>('MONGO_URI') || configService.get<string>('AZURE_COSMOS_CONNECTIONSTRING'),
-        ssl: configService.get<string>('NODE_ENV') === 'production',
-        retryWrites: false, // Importante para Cosmos DB
-      }),
-      inject: [ConfigService],
+    ConfigModule.forRoot(), // Carga las variables de entorno
+    MongooseModule.forRoot(process.env.AZURE_COSMOS_CONNECTIONSTRING, {
+      ssl: true, // Importante para Cosmos DB
+      retryWrites: false, // Cosmos DB no soporta "retryWrites"
     }),
     UsersModule,
     ChessModule,
-    BotsModule,
+    BotsModule, // Módulo de los servicios de ajedrez
   ],
   controllers: [],
   providers: [],
