f0dffa128f993c234d70c0971d3cd2c92ab67e3a
geoffrey0pv <geoffreypv00@gmail.com>
2025-01-16 19:47:40 -0500

    feat: Stockfish services half-implemented

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,8 @@ import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { ChessModule } from './chess/chess.module';
 import { BotsModule } from './bots/bots.module';
+import { StockfishServiceService } from './stockfish-service/stockfish-service.service';
+import { AnalysisModule } from './analysis/analysis.module';
 
 @Module({
   imports: [
@@ -14,9 +16,54 @@ import { BotsModule } from './bots/bots.module';
     }),
     UsersModule,
     ChessModule,
-    BotsModule, // Módulo de los servicios de ajedrez
+    BotsModule,
+    AnalysisModule, // Módulo de los servicios de ajedrez
+  ],
+  controllers: [],
+  providers: [StockfishServiceService],
+})
+export class AppModule {}
+
+
+
+/***  
+ * 
+ * 
+ * import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
+import { ChessModule } from './chess/chess.module';
+import { BotsModule } from './bots/bots.module';
+
+@Module({
+  imports: [
+    // ConfigModule cargará la configuración adecuada según NODE_ENV
+    ConfigModule.forRoot({
+      envFilePath: [
+        `.env`, // Archivo genérico
+        `src/config/${process.env.AZURE_COSMOS_CONNECTIONSTRING || 'development'}.env`, // Según el entorno
+      ],
+      isGlobal: true, // Hace accesible la configuración globalmente
+    }),
+    // Configura Mongoose dinámicamente según la configuración
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URI') || configService.get<string>('AZURE_COSMOS_CONNECTIONSTRING'),
+        ssl: configService.get<string>('NODE_ENV') === 'production',
+        retryWrites: false, // Importante para Cosmos DB
+      }),
+      inject: [ConfigService],
+    }),
+    UsersModule,
+    ChessModule,
+    BotsModule,
   ],
   controllers: [],
   providers: [],
 })
 export class AppModule {}
+
+
+ */
